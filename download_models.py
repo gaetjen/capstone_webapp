c08a94d1606b7b2984fc93bf9376f5f24b0ec9d4
jo <johannesgaetjen@gmail.com>
2018-07-07 14:35:49 +0200

    added file downloading

---
+++ download_models.py
@@ -0,0 +1,47 @@
+# taken from stackoverflow: https://stackoverflow.com/a/39225272/5388004
+import requests
+
+
+def download_file_from_google_drive(file_id, destination):
+    url = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(url, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = session.get(url, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+if __name__ == "__main__":
+    file_ids = ['1zuE9g8jYiSBus_nLG0u7Xfi_rscMGhu8',
+                '1HLAjghVXZsreVrFg02rT4Dn9w35aOMqJ',
+                '1K0CyCN7YQucVxfV5eL00XBqu3-8G5p2Z']
+    file_names = ['classifier-damaged-xception.h5',
+                  'pca_svc_tuned.pk',
+                  'vgg16_notop.h5']
+    destination_folder = 'models/'
+    destinations = [destination_folder + fn for fn in file_names]
+    for f_id, dst in zip(file_ids, destinations):
+        download_file_from_google_drive(f_id, dst)
